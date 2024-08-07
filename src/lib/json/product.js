27ae078ac77e9f6fa46f17bdaf50700b8650cb9a
callow <73476496+openscallow@users.noreply.github.com>
2024-08-07 18:00:57 +0530

    Update product.js

--- src/lib/json/product.js
+++ src/lib/json/product.js
@@ -7,5 +7,24 @@ export let productDatabase ={
         currentPrice: 70,
         previousPrice: 75,
         id:0
-    }  
+    },  
+    1:{
+        img: "https://m.media-amazon.com/images/I/51DeDlq-imL.jpg",
+        compnay: "Kangaro",
+        name: "kangaro Staple Pin no.10",
+        description: "Package includes 1 packs of Kangaro NO. 10-1M heavy-duty, zinc-coated, rust-resistant steel staple pins (1000 pins each), durable and long-lasting for efficient and error-free usage.",
+        currentPrice: 5,
+        previousPrice: 10,
+        id:1
+    },
+    2:{
+        img: "https://m.media-amazon.com/images/I/61imoDGpnxL._SY355_.jpg",
+        compnay: "Kangaro",
+        name: "faber castell highlighter [yellow]",
+        description: "Super fluorescent highlighter with longer writing length, a special non-porous polypropylene body for extended shelf life, uniform ink flow from unique ink injection technology, washable non-toxic ink, a pocket clip for easy carrying, and available in five colors: yellow, pink, green, orange, and blue.",
+        currentPrice: 20,
+        previousPrice: 25,
+        id:2
+    }
+    
   }
